81058290fcc5b4f6931ec28b1c6e2a80b29ea78d
Khambar Dussaliyev <dusaliyev@kolesa.kz>
2023-12-08 21:06:01 +0600

    add harpoon shortcuts to files in list

--- lua/remaps/harpoon.lua
+++ lua/remaps/harpoon.lua
@@ -15,3 +15,19 @@ end)
 vim.keymap.set("n", "<leader>hc", function()
 	harpoon:list():clear()
 end)
+
+vim.keymap.set("n", "<leader>hq", function()
+	harpoon:list():select(1)
+end)
+
+vim.keymap.set("n", "<leader>hw", function()
+	harpoon:list():select(2)
+end)
+
+vim.keymap.set("n", "<leader>he", function()
+	harpoon:list():select(3)
+end)
+
+vim.keymap.set("n", "<leader>hr", function()
+	harpoon:list():select(4)
+end)
