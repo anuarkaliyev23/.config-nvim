6505c9e65ca0b9ee94957bca7c226bddce15bd42
Khambar Dussaliyev <dusaliyev@kolesa.kz>
2023-12-21 17:42:35 +0600

    remove custom config

--- lua/config/randomness.lua
+++ lua/config/randomness.lua
@@ -1,19 +1,3 @@
 local randomness = require("randomness")
-local config = {
-	logLevel = 'info',
-	defaults = {
-		integers = {
-			min = 0,
-			max = 100,
-		},
 
-		arrays = {
-			delimiter = ",",
-			openingBracket = "[",
-			closingBracket =  "]",
-			length = 10,
-		}
-	}
-}
-
-randomness:setup(config)
+randomness:setup()
