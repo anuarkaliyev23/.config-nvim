e5344b79604427160dc31fb940235a699737ba1a
Khambar Dussaliyev <anuarkaliyev23@gmail.com>
2024-09-22 16:17:22 +0500

    add test stop shortcut

--- lua/code/testing/neotest.lua
+++ lua/code/testing/neotest.lua
@@ -24,7 +24,7 @@ wk.add({
 	{ "<leader>tr", neotest.run.run,                                    desc = "Run test",                  mode = "n" },
 	{ "<leader>tf", function() neotest.run.run(vim.fn.expand("%")) end, desc = "Run all tests in file",     mode = "n" },
 	{ "<leader>td", function() neotest.run.run({ strategy = "dap" }) end, desc = "Debug test",              mode = "n" },
-	{ "<leader>ts", neotest.run.stop,                                   desc = "Stop test",                 mode = "n" },
+	{ "<leader>tt", neotest.run.stop,                                   desc = "Stop/Terminate test",                 mode = "n" },
 	{ "<leader>ta", neotest.run.attach,                                 desc = "Attach to test",            mode = "n" },
 	{ "<leader>tp", neotest.output_panel.toggle,                        desc = "Toggle tests output panel", mode = "n" },
 })
